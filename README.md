2b2045496ab42e41cd8bab4bf313876a93e9a4a4
worldluoji <qq1132416@outlook.com>
2022-12-12 10:07:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vue lowcode
+
+## install
+yarn
+
+## start
+yarn dev
+
+## TODOS
+1. 原子属性
+2. 打包
+3. 接口配置
+4. 远程引入卡片
---
+++ src/components/Offer.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Card } from 'vant'
+defineProps({
+    props: Object,
+    className: String,
+})
+</script>
+
+<template>
+<van-card
+  num="1"
+  price="2999.00"
+  desc="New IPAD 10.8inc"
+  title="IPAD 2022"
+  thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg"
+/>
+</template>
