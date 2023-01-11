18a2f7fa39ef56030bf763d1ddab19c8b75c3b68
worldluoji <qq1132416@outlook.com>
2023-01-11 14:30:55 +0800

    refacotr: 暂不外部化处理vue

--- packages/elements/vite.config.js
+++ packages/elements/vite.config.js
@@ -17,17 +17,17 @@ export default defineConfig({
       name: 'OriginElements',
       // the proper extensions will be added
       fileName: 'origin-elements'
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue'
-        }
-      }
     }
+    // rollupOptions: {
+    //   // 确保外部化处理那些你不想打包进库的依赖
+    //   external: ['vue'],
+    //   output: {
+    //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+    //     globals: {
+    //       vue: 'Vue'
+    //     }
+    //   }
+    // }
   },
   plugins: [
     vue(),
