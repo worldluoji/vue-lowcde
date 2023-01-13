5f5dd0dde23a2d4e232fe07c85625166f5d529db
worldluoji <qq1132416@outlook.com>
2023-01-13 16:35:25 +0800

    fix: eslint报错修复

--- packages/platform/vite.config.js
+++ packages/platform/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
@@ -15,7 +15,7 @@ import autoprefixer from 'autoprefixer';
 const prefix = 'monaco-editor/esm/vs';
 
 // https://vitejs.dev/config/
-export default ({ mode }) =>
+export default () =>
   defineConfig({
     plugins: [
       vue(),
