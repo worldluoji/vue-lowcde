5c808d5cf69b2db8c6fbd8a207136bb1d1b1dd23
worldluoji <qq1132416@outlook.com>
2023-01-17 11:13:09 +0800

    refactor:  export简化代码

--- packages/helper/src/index.js
+++ packages/helper/src/index.js
@@ -1,13 +1,6 @@
-import atomicManager from './AtomicManager/AtomicManager';
-import dragManager from './dragmanager/dragmanager';
-import UUID from './UUID/uuid';
-import Request from './plugins/request';
-import render from './Render/Render.vue';
-import JavaScriptEditor from './editor/JavaScriptEditor.vue';
-
-export const AtomicManager = atomicManager;
-export const DragManager = dragManager;
-export const uuid = UUID;
-export const request = Request;
-export const Render = render;
-export const JSEditor = JavaScriptEditor;
+export { default as AtomicManager } from './AtomicManager/AtomicManager';
+export { default as DragManager } from './dragmanager/dragmanager';
+export { default as uuid } from './UUID/uuid';
+export { default as request } from './plugins/request';
+export { default as Render } from './Render/Render.vue';
+export { default as JSEditor } from './editor/JavaScriptEditor.vue';
