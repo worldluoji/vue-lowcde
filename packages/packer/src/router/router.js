8985e753f56df6da46ea4be57e6cfe57fcc728cd
worldluoji <qq1132416@outlook.com>
2023-01-18 17:19:12 +0800

    feat: 动态组织路由简单实现

--- packages/packer/src/router/router.js
+++ packages/packer/src/router/router.js
@@ -1,16 +1,54 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('../view/DynamicRender.vue')
-  }
-];
+const routes = [];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+});
+
+// 模拟从后端加载首页数据, 实际应该通过appId从后端获取。
+const getHost = async () => {
+  return new Promise((reslove) => {
+    reslove({ path: '/', name: 'home', pageId: 3 });
+  });
+};
+
+const host = await getHost();
+// console.log(111, host);
+if (host) {
+  router.addRoute({
+    path: host.path,
+    name: host.name,
+    component: () => import('../view/DynamicRender.vue'),
+    props: { pageId: host.pageId }
+  });
+}
+
+const generateRoute = (to, name) => {
+  return {
+    name: name,
+    path: to.path,
+    component: () => import('../view/DynamicRender.vue'),
+    props: { pageId: to.query.pageId }
+  };
+};
+
+router.beforeEach((to) => {
+  // console.log(111, to.name, to.fullPath, to.path);
+  if (to.path === '/') {
+    return;
+  }
+  let name = to.name;
+  if (!name) {
+    name = to.path.slice(1);
+  }
+  if (!router.hasRoute(name)) {
+    // console.log(222, to.name, to.fullPath, to.path);
+    router.addRoute(generateRoute(to, name));
+    // 触发重定向
+    return to.fullPath;
+  }
 });
 
 export default router;
--- packages/packer/src/view/DynamicRender.vue
+++ packages/packer/src/view/DynamicRender.vue
@@ -1,16 +1,48 @@
 <template>
-  <Render :content="content" />
+  <div>
+    <Render :content="content" />
+    <p>{{ p.pageId }}</p>
+    <el-button @click="test">跳转测试</el-button>
+  </div>
 </template>
 
+<!-- <script>
+export default {
+  props: ['pageId']
+};
+</script> -->
+
 <script setup>
-import { onBeforeMount, inject, ref } from 'vue';
+import { onBeforeMount, inject, ref, watch } from 'vue';
 import { Render } from '@lowcode/helper';
+import { useRouter } from 'vue-router';
 const $request = inject('$request');
-let content = ref([]);
-onBeforeMount(async () => {
-  let res = await $request.get('/v1/meta/get?pageId=3');
-  if (res && res.content) {
-    content.value = JSON.parse(res.content);
+const p = defineProps(['pageId']);
+
+const content = ref([]);
+const updateData = async (pageId) => {
+  if (pageId) {
+    let res = await $request.get(`/v1/meta/get?pageId=${pageId}`);
+    if (res && res.content) {
+      content.value = JSON.parse(res.content);
+    }
   }
+};
+onBeforeMount(async () => {
+  updateData(p.pageId);
 });
+
+watch(p, (newVal) => {
+  updateData(newVal.pageId);
+});
+
+const router = useRouter();
+const test = () => {
+  console.log(p.pageId);
+  if (p.pageId == 2) {
+    router.push({ path: '/first', query: { pageId: 3 } });
+  } else {
+    router.push({ path: '/second', query: { pageId: 2 } });
+  }
+};
 </script>
