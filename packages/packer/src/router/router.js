6e9692131c299eb760819f86b80f9c7d8f0883d2
worldluoji <qq1132416@outlook.com>
2023-01-19 09:53:23 +0800

    refacotr: router整合修改

--- packages/packer/src/router/router.js
+++ packages/packer/src/router/router.js
@@ -7,7 +7,7 @@ const router = createRouter({
   routes
 });
 
-// 模拟从后端加载首页数据, 实际应该通过appId从后端获取。
+// 模拟从后端加载首页数据, 实际应该通过appId从后端获取。这里实际可以把所有路由都加载了，后续就不用再动态加，只用处理跳转登陆逻辑（权限控制）
 const getHost = async () => {
   return new Promise((reslove) => {
     reslove({ path: '/', name: 'home', pageId: 3 });
@@ -36,19 +36,26 @@ const generateRoute = (to, name) => {
 
 router.beforeEach((to) => {
   // console.log(111, to.name, to.fullPath, to.path);
-  if (to.path === '/') {
-    return;
-  }
+  console.log(router.getRoutes());
   let name = to.name;
   if (!name) {
+    // 如果已经是'/'直接返回了
+    if (to.path === '/') {
+      return;
+    }
     name = to.path.slice(1);
   }
-  if (!router.hasRoute(name)) {
-    // console.log(222, to.name, to.fullPath, to.path);
-    router.addRoute(generateRoute(to, name));
-    // 触发重定向
-    return to.fullPath;
+
+  // 路由已经存在，直接走路由即可
+  if (router.hasRoute(name)) {
+    return;
   }
+
+  // console.log(222, to.name, to.fullPath, to.path);
+  // 路由不存在，就根据跳转的path动态添加路由
+  router.addRoute(generateRoute(to, name));
+  // 触发重定向
+  return to.fullPath;
 });
 
 export default router;
