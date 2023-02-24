0e2082fe840222b39738f04c807ba897ed175c2e
worldluoji <qq1132416@outlook.com>
2023-02-24 16:23:00 +0800

    refactor: 基础组件库目录优化

---
+++ packages/elements/src/components/mobile/index.js
@@ -0,0 +1,27 @@
+const modules = import.meta.glob('./**/*.vue', { eager: true });
+let components = {};
+Object.values(modules).forEach((m) => {
+  components[m.default.__name] = m.default;
+});
+// console.log(components);
+export default (Vue) => {
+  for (let k in components) {
+    Vue.component(k, components[k]);
+  }
+};
+
+export const BasicMobileComponents = {
+  ...components
+};
+
+// 组件信息
+const modulesInfo = import.meta.glob('./**/*.json', { eager: true });
+let infos = {};
+Object.values(modulesInfo).forEach((m) => {
+  infos[m.name] = m.default;
+});
+// console.log(infos);
+
+export const BasicMobileComponentsInfo = {
+  ...infos
+};
