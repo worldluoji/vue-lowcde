4dcbec570129981cd283e35515c7df2a48fd5027
worldluoji <qq1132416@outlook.com>
2023-06-29 16:16:04 +0800

    fix: runtime import elements seperatly

--- packages/runtime/src/main.js
+++ packages/runtime/src/main.js
@@ -1,22 +1,31 @@
 import App from './App.vue';
 import router from './router/router';
-import elements from '@lowcode/elements';
 import { AtomicManager } from '@lowcode/helper';
 import { request } from '@lowcode/request';
 
 const pinia = Pinia.createPinia();
 const atomicManager = new AtomicManager();
 
-Vue.createApp(App)
+const app = Vue.createApp(App)
   .directive('atomicattr', (el, binding) => {
     atomicManager.setMargin(el, binding);
   })
   .use(router)
   .use(pinia)
-  .use(elements.BasicMobileComponentsIn)
-  .use(elements.BasicWebComponentsIn)
-  .use(elements.ContainerComponentsIn)
   .use(request)
   .use(ElementPlus)
-  .use(vant)
-  .mount('#app');
+  .use(vant);
+
+const s = document.createElement('script');
+// 这里后面要改为从后端获取，就可以实现指向不同的引擎
+const url = `${import.meta.env.VITE_RESOURCE_URL}`;
+s.type = 'text/javascript';
+s.src = url;
+s.onload = () => {
+  const resources = window.StandardElements;
+  app.use(resources.default.BasicMobileComponentsIn);
+  app.use(resources.default.BasicWebComponentsIn);
+  app.use(resources.default.ContainerComponentsIn);
+  app.mount('#app');
+};
+document.body.appendChild(s);
