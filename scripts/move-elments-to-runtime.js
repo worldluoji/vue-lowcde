6048e3c58d1939026f74a0001e9c34cbfc189035
worldluoji <qq1132416@outlook.com>
2023-07-25 14:17:52 +0800

    refacor: delete dest dir before copy

--- scripts/move-elments-to-runtime.js
+++ scripts/move-elments-to-runtime.js
@@ -1,11 +1,18 @@
-import { cp } from 'node:fs';
+import { cp, rm } from 'node:fs';
 
 const src_dir = './packages/elements/dist/__StandardElements__';
 const dest_dir = './packages/runtime/dist/static/__StandardElements__';
 
-cp(src_dir, dest_dir, { recursive: true }, (err) => {
+rm(dest_dir, { recursive: true, force: true }, (err) => {
   if (err) {
     console.error(err);
-    throw new Error('copy elements to runtime error');
+    throw new Error(`delete dest dir ${dest_dir} error`);
+  } else {
+    cp(src_dir, dest_dir, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+        throw new Error('copy elements to runtime error');
+      }
+    });
   }
 });
