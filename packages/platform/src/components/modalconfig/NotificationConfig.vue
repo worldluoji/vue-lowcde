b204f32bb7f2231fd15b64b85178d7f72f390d72
worldluoji <qq1132416@outlook.com>
2023-08-02 08:37:39 +0800

    feat: add notification configuration page

---
+++ packages/platform/src/components/modalconfig/NotificationConfig.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-form-item label="通知类型" :label-width="p.formLabelWidth">
+      <el-select v-model="form.notiType" placeholder="请选择通知类型">
+        <el-option label="info" value="info" />
+        <el-option label="error" value="error" />
+        <el-option label="warning" value="warning" />
+        <el-option label="success" value="success" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="通知标题" :label-width="p.formLabelWidth">
+      <el-input v-model="form.notiTitle" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="通知内容" :label-width="p.formLabelWidth">
+      <el-input v-model="form.notiContent" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="通知位置" :label-width="p.formLabelWidth">
+      <el-select v-model="form.notiPosition" placeholder="请选择通知位置">
+        <el-option label="bottom-right" value="bottom-right" />
+        <el-option label="bottom-left" value="bottom-left" />
+        <el-option label="top-left" value="top-left" />
+        <el-option label="top-right" value="ttop-right" />
+      </el-select>
+    </el-form-item>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+const p = defineProps({
+  formLabelWidth: {
+    type: String,
+    default: '140px'
+  },
+  newModalForm: {
+    type: Object,
+    required: true
+  }
+});
+
+const form = reactive(p.newModalForm);
+</script>
--- packages/platform/src/view/ModalConfig.vue
+++ packages/platform/src/view/ModalConfig.vue
@@ -6,6 +6,8 @@
           <template #header>
             <div class="card-header">
               <span>弹窗列表</span>
+              <el-button text @click="newModalVisible = true">+</el-button>
+              <el-button v-show="selected" text @click.stop="">-</el-button>
             </div>
           </template>
           <div
@@ -19,26 +21,67 @@
         </el-card>
       </el-aside>
       <el-main>
-        <div class="modal-config">
-          <div v-if="seleted === 'notification'">notification</div>
-          <div v-if="seleted === 'message'">message</div>
-          <div v-if="seleted === 'dialog'">dialog</div>
+        <div v-if="selected" class="modal-config">
+          <div v-if="selected.type === 'notification'">notification</div>
+          <div v-if="selected.type === 'message'">message</div>
+          <div v-if="selected.type === 'dialog'">dialog</div>
         </div>
       </el-main>
     </el-container>
+    <el-dialog v-model="newModalVisible" title="新建弹窗">
+      <el-form :model="newModalForm">
+        <el-form-item label="名称" :label-width="formLabelWidth">
+          <el-input v-model="newModalForm.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="类型" :label-width="formLabelWidth">
+          <el-select v-model="newModalForm.type" placeholder="请选择类型">
+            <el-option label="notification" value="notification" />
+            <el-option label="message" value="message" />
+            <el-option label="dialog" value="dialog" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="描述" :label-width="formLabelWidth">
+          <el-input v-model="newModalForm.desc" autocomplete="off" />
+        </el-form-item>
+        <div v-if="newModalForm.type === 'notification'">
+          <NotificationConfig
+            :form-label-width="formLabelWidth"
+            :new-modal-form="newModalForm"
+          />
+        </div>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="newModalVisible = false">取消</el-button>
+          <el-button type="primary" @click="addModal"> 确认 </el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
-import { inject, ref } from 'vue';
+import { inject, ref, reactive } from 'vue';
 import { storeToRefs } from 'pinia';
+import NotificationConfig from '../components/modalconfig/NotificationConfig.vue';
 const modalStore = inject('$modalStore');
 const modals = storeToRefs(modalStore());
 const modalList = ref((modals.get || {}).value);
-const seleted = ref();
+const selected = ref();
 
 const clickModal = (m) => {
-  seleted.value = m;
+  selected.value = m;
+};
+
+const newModalVisible = ref(false);
+const formLabelWidth = '140px';
+let newModalForm = reactive({});
+
+const addModal = () => {
+  // TODO 调用后端接口存入数据库， 并更新store
+  console.log(newModalForm);
+  newModalVisible.value = false;
+  newModalForm = reactive({});
 };
 </script>
 
@@ -64,5 +107,18 @@ const clickModal = (m) => {
 
 .aside {
   width: 250px;
+}
+
+.el-button--text {
+  margin-right: 15px;
+}
+.el-select {
+  width: 300px;
+}
+.el-input {
+  width: 300px;
+}
+.dialog-footer button:first-child {
+  margin-right: 10px;
 }
 </style>
